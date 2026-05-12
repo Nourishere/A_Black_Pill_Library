45438948b8ba9168dc7e27feacc02f604dc593d3
Nour Nawar <nournawar5@gmail.com>
2026-05-12 21:01:39 +0300

    Add device-level interrupt source/header
    These functions depend on lower layers. I tried structuing them for
    portability. I could have done better if I only had a slight affection
    for enums. Too ugly for me.

---
+++ Inc/drivers/interrupt.h
@@ -0,0 +1,128 @@
+/*
+ * STM32F401 specific interrupt header
+ * Written with portability in mind
+ * Date: Fri May 8 08:10:20 PM EEST 2026
+ * Author: Nour Nawar <nournawar5@gmail.com>
+ */
+
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+
+#include <stdint.h>
+#include <core/scb.h>
+#include <core/nvic.h>
+#include <core/cpu.h>
+
+// Total priority bits for the device
+#define INTERRUPT_PRIO_BITS 4
+
+/*
+ * All IRQ lines on the device 
+ */
+typedef enum {
+		WWDG,
+		EXTI16 = 1, PVD = 1,
+		EXTI21 = 2, TAMP_STAMP = 2,
+		EXTI22 = 3, RTC_WKUP = 3,
+		FLASH, RCC, EXTI0,
+		EXTI1, EXTI2, EXTI3, EXTI4,
+		DMA1_Stream0, DMA1_Stream1,
+		DMA1_Stream2, DMA1_Stream3, DMA1_Stream4,
+		DMA1_Stream5, DMA1_Stream6, ADC,
+		// Reserved IRQ lines here
+		EXTI9_5 = 23,
+		TIM1_BRK_TIM9, TIM1_UP_TIM10,
+		TIM1_TRG_COM = 26, TIM11 = 26,
+		TIM1_CC, TIM2, TIM3,
+		TIM4, I2C1_EV, I2C1_ER,
+		I2C2_EV, I2C2_ER, SPI1,
+		SPI2, USART1, USART2,
+		// Reserved IRQ lines here
+		EXTI15_10 = 40,
+        EXTI17 = 41 , RTC_Alarm = 41,
+		EXTI18 = 42, OTG_FS_WKUP = 42,
+		// Reserved IRQ lines here
+		DMA1_Stream7 = 47,
+		// Reserved IRQ line here
+        SDIO = 49,
+		TIM5, SPI3,
+		// Reserved IRQ line here
+        DMA2_Stream0 = 56,
+		DMA2_Stream1, DMA2_Stream2, DMA2_Stream3,
+		DMA2_Stream4,
+		// Reserved IRQ line here
+		OTG_FS = 67,
+        DMA2_Stream5, DMA2_Stream6, DMA2_Stream7,
+        USART6, I2C3_EV, I2C3_ER,
+		// Reserved IRQ line here
+        FPU = 81,
+		// Reserved IRQ line here
+		PI4 = 84
+} IRQn_t;
+
+/*
+ * Initialize interrupts by doing the following:
+ * - Setting the preempt/subgroup.
+ * - Disabling interrupts completely.
+ * - Clearning priority and pending states.
+ *
+ * NOTE: This function gives you the possibility to either use
+ * 			or ignore sub priorities. It should be called once
+ * 			at initialization time.
+ *
+ * groupbits: The number of group priority bits.
+ * subbits: The number of sub priority bits.
+ *
+ * NOTE: if you wish NOT to use any sub priority, just 
+ * 			set sublevels to 0 and grouplevels to INTERRUPT_PRIO_BITS.
+ */
+uint8_t interrupt_init(uint8_t groupbits);
+
+/*
+ * Set the priority of an interrupt line
+ *
+ * NOTE: The interrupt priority you give is the full 8-bit priority
+ *
+ * priority: The priority you wish to give out
+ */
+uint8_t interrupt_set_priority(IRQn_t line, uint8_t priority);
+
+/*
+ * Fetch the current priority on an IRQ line
+ * 
+ * Use a pointer for return
+ */
+uint8_t interrupt_get_priority(IRQn_t line, uint8_t * priority);
+
+/*
+ * Set the pending state for an IRQ line
+ */
+uint8_t interrupt_set_pending(IRQn_t line);
+
+/*
+ * Clear the pending state for an IRQ line
+ */
+uint8_t interrupt_clear_pending(IRQn_t line);
+
+/*
+ * Is the IRQ line pending service? 
+ * Return 1 if it is and 0 if is not
+ */
+uint8_t interrupt_is_pending(IRQn_t line);
+
+/*
+ * Is the IRQ line being served? 
+ * Return 1 if it is and 0 if is not
+ */
+uint8_t interrupt_is_active(IRQn_t line);
+/*
+ * Enable an IRQ line
+ */
+uint8_t interrupt_enable_IRQ(IRQn_t line);
+
+/*
+ * Disable an IRQ line
+ */
+uint8_t interrupt_disable_IRQ(IRQn_t line);
+
+#endif // INTERRUPT_H
---
+++ drivers/interrupt.c
@@ -0,0 +1,186 @@
+/*
+ * STM32F401-specific interrupt API implementation
+ *
+ * Date: Tue May 12 03:46:08 PM EEST 2026
+ * Author: Nour Nawar <nournawar5@gmail.com>
+ */
+
+#include <drivers/interrupt.h>
+
+static uint8_t interrupt_check_IRQn(IRQn_t line);
+
+/*
+ * Initialize interrupts by doing the following:
+ * - Setting the preempt/subgroup.
+ * - Disabling interrupts completely.
+ * - Clearning priority and pending states.
+ *
+ * NOTE: Clear interrupt masking after calling this function to 
+ * 			ensure interrupt are unmasked.
+ *
+ * NOTE: This function gives you the possibility to either use
+ * 			or ignore sub priorities. It should be called once
+ * 			at initialization time.
+ *
+ * groupbits: The number of preempt priority bits.
+ *
+ * NOTE: if you wish NOT to use any sub priority, just 
+ * 			set groupbits to INTERRUPT_PRIO_BITS.
+ *
+ * Return 0 upon success and 1 otherwise
+ *
+ */
+uint8_t interrupt_init(uint8_t groupbits)
+{
+	// Set priority grouping
+	if(groupbits < 1 || groupbits > INTERRUPT_PRIO_BITS)
+		return 1;
+
+	NVIC_disable_all();
+	NVIC_clear_all_pending();
+	NVIC_reset_all_priorities();
+
+	SCB_set_priority_grouping(7U - groupbits);
+	return 0;
+}
+
+/*
+ * Set the priority of an interrupt line
+ *
+ * NOTE: The interrupt priority you give is the full 8-bit priority
+ *
+ * priority: The priority you wish to set 
+ *
+ * Return 0 upon success and 1 otherwise
+ *
+ */
+uint8_t interrupt_set_priority(IRQn_t line, uint8_t priority)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	NVIC_set_priority(line, priority);
+
+	return 0;
+}
+
+/*
+ * Fetch the current priority on an IRQ line
+ * 
+ * priority: A pointer to store the priority
+ *
+ * Return 0 upon success and 1 otherwise
+ *
+ */
+uint8_t interrupt_get_priority(IRQn_t line, uint8_t * priority)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	*priority = NVIC_get_priority(line);
+
+	return 0;
+}
+
+/*
+ * Set the pending state for an IRQ line
+ *
+ * Return 0 upon success and 1 otherwise
+ *
+ */
+uint8_t interrupt_set_pending(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	NVIC_set_pending(line);
+
+	return 0;
+}
+
+/*
+ * Clear the pending state for an IRQ line
+ *
+ * Return 0 upon success and 1 otherwise
+ *
+ */
+uint8_t interrupt_clear_pending(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	NVIC_clear_pending(line);
+
+	return 0;
+}
+
+/*
+ * Is the IRQ line pending service? 
+ *
+ * Return 1 if it is and 0 if is not
+ *
+ */
+uint8_t interrupt_is_pending(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+
+	return NVIC_is_pending(line);
+}
+
+/*
+ * Is the IRQ line being served? 
+ *
+ * Return 1 if it is and 0 if is not
+ *
+ */
+uint8_t interrupt_is_active(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+
+	return NVIC_is_active(line);
+}
+/*
+ * Enable an IRQ line
+ *
+ * Return 0 upon success and 1 otherwise 
+ *
+ */
+uint8_t interrupt_enable_IRQ(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	NVIC_enable_IRQ(line);
+
+	return 0;
+}
+
+/*
+ * Disable an IRQ line
+ *
+ * Return 0 upon success and 0 otherwise 
+ *
+ */
+uint8_t interrupt_disable_IRQ(IRQn_t line)
+{
+	if(interrupt_check_IRQn(line))
+		return 1;
+	NVIC_disable_IRQ(line);
+
+	return 0;
+}
+
+
+/* Helper
+ * Check if the IRQn_t input is valid
+ *
+ * Return 0 if it is valid and 1 otherwise
+ *
+ */
+static uint8_t interrupt_check_IRQn(IRQn_t line)
+{
+	if(line > PI4)
+		return 1;
+	if((line > 18 && line < 23) || (line == 39) || (line > 42 && line < 47)
+			|| (line == 48) || (line > 51 && line < 56) || (line > 60 && line <67)
+			|| (line > 73 && line < 81) || (line > 81 && line < 84))
+		return 1;   
+	return 0;
+}
